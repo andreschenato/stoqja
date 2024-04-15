e7d115c5d109494efa23fd2ce69803b6e6b66f29
André Vitor <andreschenato@gmail.com>
2024-04-15 20:07:28 -0300

    Configuração da tela de login e outros ajustes

---
+++ lib/login/login_widget.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:stoque_ja/login/user_select.dart';
+import 'package:stoque_ja/login/valida_login.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({super.key});
+  @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  String? usuario;
+  final senha = TextEditingController();
+
+  @override
+  void dispose() {
+    senha.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(
+        maxHeight: 500,
+        maxWidth: 1000,
+        minHeight: 100,
+        minWidth: 200,
+      ),
+      width: MediaQuery.of(context).size.width / 2,
+      height: MediaQuery.of(context).size.height / 2,
+      padding: const EdgeInsets.all(30.0),
+      decoration: const BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.all(Radius.circular(40))),
+      child: Column(
+        children: [
+          Container(
+            constraints: const BoxConstraints(
+              maxWidth: 1000,
+              minWidth: 200,
+            ),
+            child: UserSelect(onUserSelected: (selectedUser) {
+              setState(() {
+                usuario = selectedUser;
+              });
+            }),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            controller: senha,
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: 'Senha',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          Container(
+            constraints: const BoxConstraints(
+              maxHeight: 70,
+              maxWidth: 1000,
+              minHeight: 60,
+              minWidth: 200,
+            ),
+            height: double.infinity,
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+              onPressed: () async {
+                bool? isValid;
+                isValid = await validaLogin(usuario.toString(), senha.text);
+                if (isValid) {
+                  Navigator.pushNamed(context, '/Menu');
+                } else {
+                  Center(child: Text('Usuário ou senha inválidos'));
+                }
+              },
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 50),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/login/user_select.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:stoque_ja/login/user_list.dart';
+
+class UserSelect extends StatefulWidget {
+  final void Function(String?)? onUserSelected;
+  const UserSelect({super.key, this.onUserSelected});
+
+  @override
+  State<UserSelect> createState() => _UserSelectState();
+}
+
+class _UserSelectState extends State<UserSelect> {
+  Future? _userList;
+  String? _selectedUser;
+
+  String? get selectedUser => _selectedUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _userList = userList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: const BoxConstraints(
+        maxHeight: 70,
+        maxWidth: 1000,
+        minHeight: 60,
+        minWidth: 200,
+      ),
+      height: double.infinity,
+      width: double.infinity,
+      child: FutureBuilder(
+        future: _userList,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              List<String> userList = snapshot.data;
+              return DropdownButtonFormField<String>(
+                isExpanded: true,
+                decoration: InputDecoration(
+                  label: const Text('Usuário'),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4)),
+                ),
+                hint: const Text("Selecione seu usuário"),
+                value: _selectedUser,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedUser = value!;
+                    widget.onUserSelected?.call(_selectedUser);
+                  });
+                },
+                items: userList.map((String value) {
+                  return DropdownMenuItem(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              );
+            }
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
