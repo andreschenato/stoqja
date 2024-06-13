97e8820a8134aa23c10c2c82e3e97d4b28ee2fb1
André Vitor <andreschenato@gmail.com>
2024-06-13 16:24:59 -0300

    Adicionada permissões no menu e drawer

--- lib/menu_principal/menu_buttons.dart
+++ lib/menu_principal/menu_buttons.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:stoque_ja/menu_principal/button.dart';
 import 'package:stoque_ja/rotas/routes.dart';
+import 'package:stoque_ja/widgets/logged_user.dart';
 
 // Widget que implementa os botões do widget "GridButton"
 // para criar a grade de botões do menu
@@ -9,98 +11,86 @@ class MenuButtons extends StatelessWidget {
   const MenuButtons({super.key});
   @override
   Widget build(BuildContext context) {
+    bool isAdm;
     int axisItemsCount;
-    MediaQuery.of(context).size.width < 650
-        ? (axisItemsCount = 2)
-        : (axisItemsCount = 3);
     double heightbox = MediaQuery.of(context).size.height;
-    final String usuario = ModalRoute.of(context)?.settings.arguments as String;
-    return Scaffold(
-      body: Container(
-        constraints: BoxConstraints(
-          maxHeight: heightbox,
-          maxWidth: 1000,
-          minHeight: 100,
-          minWidth: 400,
-        ),
-        child: GridView(
-          shrinkWrap: true,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: axisItemsCount,
-            crossAxisSpacing: 30,
-            mainAxisSpacing: 30,
-            childAspectRatio: 1.4,
+    return Consumer<LoggedUser>(
+      builder: (context, value, child) {
+        value.cargo.toString().toLowerCase() == 'administrador' ||
+                value.cargo.toString().toLowerCase() == 'gerente'
+            ? isAdm = true
+            : isAdm = false;
+        MediaQuery.of(context).size.width < 650
+            ? (axisItemsCount = 2)
+            : (isAdm ? axisItemsCount = 3 : axisItemsCount = 2);
+        return Scaffold(
+          body: Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxHeight: heightbox,
+                maxWidth: 800,
+                minHeight: 100,
+                minWidth: 400,
+              ),
+              child: GridView(
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: axisItemsCount,
+                  crossAxisSpacing: 30,
+                  mainAxisSpacing: 30,
+                  childAspectRatio: 1.4,
+                ),
+                children: [
+                  GridButton(
+                    textoBotao: "Produtos",
+                    onPressed: () {
+                      Navigator.pushNamed(context, Rota.produto);
+                    },
+                    icone: Icons.local_mall,
+                  ),
+                  if (isAdm)
+                    GridButton(
+                      textoBotao: "Funcionários",
+                      onPressed: () {
+                        Navigator.pushNamed(context, Rota.funcionario);
+                      },
+                      icone: Icons.groups,
+                    ),
+                  GridButton(
+                    textoBotao: "Mov. Estoque",
+                    onPressed: () {
+                      Navigator.pushNamed(context, Rota.estoque);
+                    },
+                    icone: Icons.moving_rounded,
+                  ),
+                  GridButton(
+                    textoBotao: "Clientes",
+                    onPressed: () {
+                      Navigator.pushNamed(context, Rota.cliente);
+                    },
+                    icone: Icons.person,
+                  ),
+                  GridButton(
+                    textoBotao: "Vendas e ordens",
+                    onPressed: () {
+                      Navigator.pushNamed(context, Rota.venda);
+                    },
+                    icone: Icons.assignment,
+                  ),
+                  if (isAdm)
+                    GridButton(
+                      textoBotao: "Fornecedores",
+                      onPressed: () {
+                        Navigator.pushNamed(context, Rota.fornecedor);
+                      },
+                      icone: Icons.local_shipping_rounded,
+                    ),
+                ],
+              ),
+            ),
           ),
-          children: [
-            GridButton(
-              textoBotao: "Produtos",
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  Rota.produto,
-                  arguments: usuario,
-                );
-              },
-              icone: Icons.local_mall,
-            ),
-            GridButton(
-              textoBotao: "Funcionários",
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  Rota.funcionario,
-                  arguments: usuario,
-                );
-              },
-              icone: Icons.groups,
-            ),
-            GridButton(
-              textoBotao: "Mov. Estoque",
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  Rota.estoque,
-                  arguments: usuario,
-                );
-              },
-              icone: Icons.moving_rounded,
-            ),
-            GridButton(
-              textoBotao: "Clientes",
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  Rota.cliente,
-                  arguments: usuario,
-                );
-              },
-              icone: Icons.person,
-            ),
-            GridButton(
-              textoBotao: "Vendas e ordens",
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  Rota.venda,
-                  arguments: usuario,
-                );
-              },
-              icone: Icons.assignment,
-            ),
-            GridButton(
-              textoBotao: "Fornecedores",
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  Rota.fornecedor,
-                  arguments: usuario,
-                );
-              },
-              icone: Icons.local_shipping_rounded,
-            ),
-          ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/widgets/drawer_component.dart
+++ lib/widgets/drawer_component.dart
@@ -10,171 +10,187 @@ class DrawerComponent extends StatelessWidget {
   const DrawerComponent({super.key, required this.tela});
   @override
   Widget build(BuildContext context) {
+    bool isAdm;
     final String selectedIndex = tela;
     final String usuario = ModalRoute.of(context)?.settings.arguments as String;
     return Drawer(
-      child: ListView(
-        padding: const EdgeInsets.only(top: 0),
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 6,
-            child: DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Color.fromRGBO(255, 117, 0, 1),
+      child: Consumer<LoggedUser>(
+        builder: (context, value, child) {
+          value.cargo.toString().toLowerCase() == 'administrador' ||
+                  value.cargo.toString().toLowerCase() == 'gerente'
+              ? isAdm = true
+              : isAdm = false;
+          return ListView(
+            padding: const EdgeInsets.only(top: 0),
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 6,
+                child: DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(255, 117, 0, 1),
+                  ),
+                  child: Consumer<LoggedUser>(
+                    builder: (context, value, child) {
+                      return Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                value.username.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                value.cargo.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
               ),
-              child: Consumer<LoggedUser>(
-                builder: (context, value, child) {
-                  return Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            value.username.toString(),
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            value.cargo.toString(),
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
+              ListTile(
+                title: const Row(
+                  children: [
+                    Icon(Icons.door_back_door),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Menu"),
+                  ],
+                ),
+                selected: selectedIndex == 'Menu',
+                onTap: () {
+                  Navigator.pushNamed(context, Rota.menu, arguments: usuario);
                 },
               ),
-            ),
-          ),
-          ListTile(
-            title: const Row(
-              children: [
-                Icon(Icons.door_back_door),
-                SizedBox(
-                  width: 10,
+              ListTile(
+                title: const Row(
+                  children: [
+                    Icon(Icons.local_mall),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Produtos"),
+                  ],
                 ),
-                Text("Menu"),
-              ],
-            ),
-            selected: selectedIndex == 'Menu',
-            onTap: () {
-              Navigator.pushNamed(context, Rota.menu, arguments: usuario);
-            },
-          ),
-          ListTile(
-            title: const Row(
-              children: [
-                Icon(Icons.local_mall),
-                SizedBox(
-                  width: 10,
+                selected: selectedIndex == 'Produtos',
+                onTap: () {
+                  Navigator.pushNamed(context, Rota.produto,
+                      arguments: usuario);
+                },
+              ),
+              if (isAdm)
+                ListTile(
+                  title: const Row(
+                    children: [
+                      Icon(Icons.groups),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Funcionários"),
+                    ],
+                  ),
+                  selected: selectedIndex == 'Funcionarios',
+                  onTap: () {
+                    Navigator.pushNamed(context, Rota.funcionario,
+                        arguments: usuario);
+                  },
                 ),
-                Text("Produtos"),
-              ],
-            ),
-            selected: selectedIndex == 'Produtos',
-            onTap: () {
-              Navigator.pushNamed(context, Rota.produto, arguments: usuario);
-            },
-          ),
-          ListTile(
-            title: const Row(
-              children: [
-                Icon(Icons.groups),
-                SizedBox(
-                  width: 10,
+              ListTile(
+                title: const Row(
+                  children: [
+                    Icon(Icons.moving_rounded),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Mov. de Estoque"),
+                  ],
                 ),
-                Text("Funcionários"),
-              ],
-            ),
-            selected: selectedIndex == 'Funcionarios',
-            onTap: () {
-              Navigator.pushNamed(context, Rota.funcionario, arguments: usuario);
-            },
-          ),
-          ListTile(
-            title: const Row(
-              children: [
-                Icon(Icons.moving_rounded),
-                SizedBox(
-                  width: 10,
+                selected: selectedIndex == 'MovEstoque',
+                onTap: () {
+                  Navigator.pushNamed(context, Rota.estoque,
+                      arguments: usuario);
+                },
+              ),
+              ListTile(
+                title: const Row(
+                  children: [
+                    Icon(Icons.person),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Clientes"),
+                  ],
                 ),
-                Text("Mov. de Estoque"),
-              ],
-            ),
-            selected: selectedIndex == 'MovEstoque',
-            onTap: () {
-              Navigator.pushNamed(context, Rota.estoque, arguments: usuario);
-            },
-          ),
-          ListTile(
-            title: const Row(
-              children: [
-                Icon(Icons.person),
-                SizedBox(
-                  width: 10,
+                selected: selectedIndex == 'Clientes',
+                onTap: () {
+                  Navigator.pushNamed(context, Rota.cliente,
+                      arguments: usuario);
+                },
+              ),
+              ListTile(
+                title: const Row(
+                  children: [
+                    Icon(Icons.assignment),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Vendas e Ordens"),
+                  ],
                 ),
-                Text("Clientes"),
-              ],
-            ),
-            selected: selectedIndex == 'Clientes',
-            onTap: () {
-              Navigator.pushNamed(context, Rota.cliente, arguments: usuario);
-            },
-          ),
-          ListTile(
-            title: const Row(
-              children: [
-                Icon(Icons.assignment),
-                SizedBox(
-                  width: 10,
+                selected: selectedIndex == 'VendasOrdens',
+                onTap: () {
+                  Navigator.pushNamed(context, Rota.venda, arguments: usuario);
+                },
+              ),
+              if (isAdm)
+                ListTile(
+                  title: const Row(
+                    children: [
+                      Icon(Icons.local_shipping_rounded),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text("Fornecedores"),
+                    ],
+                  ),
+                  selected: selectedIndex == 'Fornecedores',
+                  onTap: () {
+                    Navigator.pushNamed(context, Rota.fornecedor,
+                        arguments: usuario);
+                  },
                 ),
-                Text("Vendas e Ordens"),
-              ],
-            ),
-            selected: selectedIndex == 'VendasOrdens',
-            onTap: () {
-              Navigator.pushNamed(context, Rota.venda, arguments: usuario);
-            },
-          ),
-          ListTile(
-            title: const Row(
-              children: [
-                Icon(Icons.local_shipping_rounded),
-                SizedBox(
-                  width: 10,
+              ListTile(
+                title: const Row(
+                  children: [
+                    Icon(Icons.exit_to_app_rounded),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Sair"),
+                  ],
                 ),
-                Text("Fornecedores"),
-              ],
-            ),
-            selected: selectedIndex == 'Fornecedores',
-            onTap: () {
-              Navigator.pushNamed(context, Rota.fornecedor, arguments: usuario);
-            },
-          ),
-          ListTile(
-            title: const Row(
-              children: [
-                Icon(Icons.exit_to_app_rounded),
-                SizedBox(
-                  width: 10,
-                ),
-                Text("Sair"),
-              ],
-            ),
-            selected: selectedIndex == 'Login',
-            onTap: () {
-              Navigator.pushNamed(context, Rota.login, arguments: usuario);
-            },
-          ),
-        ],
+                selected: selectedIndex == 'Login',
+                onTap: () {
+                  Navigator.pushNamed(context, Rota.login, arguments: usuario);
+                },
+              ),
+            ],
+          );
+        },
       ),
     );
   }
